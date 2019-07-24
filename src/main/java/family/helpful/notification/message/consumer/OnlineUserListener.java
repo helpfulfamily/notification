36d424586cb16b341f3f4d14d9c0ae45d893ba67
murat.doner <murat.doner@atg.se>
2019-07-24 22:47:24 +0200

    ContentFormChange

--- src/main/java/family/helpful/notification/message/consumer/OnlineUserListener.java
+++ src/main/java/family/helpful/notification/message/consumer/OnlineUserListener.java
@@ -5,6 +5,7 @@ import family.helpful.notification.actions.EnumActionStatus;
 import family.helpful.notification.message.model.Channel;
 import family.helpful.notification.message.model.ChannelContent;
 import family.helpful.notification.message.model.User;
+import family.helpful.notification.message.model.UserJoinMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,7 @@ import org.springframework.messaging.Message;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 
@@ -26,19 +28,36 @@ public class OnlineUserListener
     @Autowired
     private SimpMessagingTemplate template;
 
-    private static List<User> onlineUserList= new ArrayList<>();
+    private static HashMap <String, List<User>> onlineUserMap= new HashMap<>();
     private static long id=3;
 
     @StreamListener(target = Sink.INPUT,  condition = "headers['action'] == 'userLoggedIn'")
-    public void userLoggedIn(Message<User> message) {
+    public void userLoggedIn(Message<UserJoinMessage> userJoinMessageMessageEnv) {
+        UserJoinMessage userJoinMessage= userJoinMessageMessageEnv.getPayload();
 
-        logger.info(" received userLoggedIn message ["+message.toString()+"] ");
+        logger.info(" received userLoggedIn message ["+userJoinMessage.getUser().username );
 
-        User user= message.getPayload();
+        User user= userJoinMessage.getUser();
 
         user.setId(++id);
 
-        onlineUserList.add(user);
+        String channelName= userJoinMessage.getChannelName();
+
+        List<User> onlineUserList= onlineUserMap.get(channelName);
+        if(onlineUserList==null){
+            onlineUserList= new ArrayList();
+
+        }
+        User filterUser = onlineUserList.stream()
+                .filter(filterUserInternal -> user.getUsername().equals(filterUserInternal.getUsername()))
+                .findAny()
+                .orElse(null);
+        if(filterUser==null){
+            onlineUserList.add(user);
+
+        }
+
+        onlineUserMap.put(channelName, onlineUserList);
 
         Message resultMessage= MessageBuilder.withPayload(onlineUserList)
                 .setHeader("userLoggedIn"
