fa79c24e9e13a13eef1e7f6992c5368c46cb72db
murat.doner <murat.doner@atg.se>
2019-09-23 15:45:56 +0200

    DialogContent

---
+++ src/main/java/family/helpful/notification/message/consumer/DialogListener.java
@@ -0,0 +1,38 @@
+package family.helpful.notification.message.consumer;
+
+
+import family.helpful.notification.actions.EnumActionStatus;
+import family.helpful.notification.message.model.DialogContent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+
+
+@EnableBinding({Sink.class})
+public class DialogListener {
+    private static final Logger logger = LoggerFactory.getLogger(DialogListener.class);
+    @Autowired
+    private SimpMessagingTemplate template;
+
+    @StreamListener(target = Sink.INPUT, condition = "headers['action'] == 'PUBLISH_DIALOG_CONTENT'")
+    public void publishProblemContent(Message<DialogContent> message) {
+
+        logger.info(" received PUBLISH_DIALOG_CONTENT [" + message.toString() + "] ");
+
+
+        Message resultMessage = MessageBuilder.withPayload(message.getPayload())
+                .setHeader("PUBLISH_DIALOG_CONTENT"
+                        , EnumActionStatus.SUCCESS.name()).build();
+
+        this.template.convertAndSend("/topic/pushNotificationDialogContent", resultMessage);
+
+
+    }
+
+}
